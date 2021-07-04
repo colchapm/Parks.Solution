c84457bac740f82192896cd81df115fdf672f648
Collin Chapman <cchap14@gmail.com>
2021-07-04 14:05:03 -0700

    create GetPark endpoint

--- Parks/Controllers/ParksController.cs
+++ Parks/Controllers/ParksController.cs
@@ -17,19 +17,35 @@ namespace Parks.Controller
       _db = db;
     }
 
+    // GET api/parks
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Park>>> Get()
     {
       return await _db.Parks.ToListAsync();
     }
 
+    // POST api/parks
     [HttpPost]
     public async Task<ActionResult<Park>> Post(Park park)
     {
       _db.Parks.Add(park);
       await _db.SaveChangesAsync();
 
-      return CreatedAtAction("Post", new { id = park.ParkId }, park);
+      return CreatedAtAction(nameof(GetPark), new { id = park.ParkId }, park);
+    }
+
+    // GET api/parks/[ParkId]
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Park>> GetPark(int id)
+    {
+      var park = await _db.Parks.FindAsync(id);
+
+      if (park == null)
+      {
+        return NotFound();
+      }
+
+      return park;
     }
   }
 }
